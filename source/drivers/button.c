6334488a8b1a8c169a003616d30f39ed80dc8334
Matias Francois <mfrancois@itba.edu.ar>
2020-10-11 14:09:31 -0300

    comments

--- source/drivers/button.c
+++ source/drivers/button.c
@@ -4,135 +4,17 @@
   @author   Grupo 2
  ******************************************************************************/
 
-/*******************************************************************************
- * INCLUDE HEADER FILES
- ******************************************************************************/
+
 #include "button.h"
 #include "SysTick.h"
+#include "gpio.h"
 
-
-/*******************************************************************************
- * ENUMERATIONS AND STRUCTURES AND TYPEDEFS
- ******************************************************************************/
-typedef  struct
-	{
-		pin_t pin;
-		enum type typefunction;
-		bool lastState;
-		int currentCount;
-		int lkpTime;
-		int typeTime;
-		bool wasLkp;
-		bool wasPressed;
-		bool wasReleased;
-	}Button_t;
-
-/*******************************************************************************
- * PRIVATE VARIABLES WITH FILE LEVEL SCOPE
- ******************************************************************************/
-static Button_t buttons[BUTTON_NUM];
 
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
 
-static void systick_callback(void);
-
-/*******************************************************************************
- *******************************************************************************
-                        GLOBAL FUNCTION DEFINITIONS
- *******************************************************************************
- ******************************************************************************/
-void buttonsInit(void)
-{
-	//agregar botones del punto h
-	SysTick_AddCallback(&systick_callback, 50); //50 ms
-}
-
-bool wasPressed(pin_t button)
-{
-	int count;
-	for(count=0;count<BUTTON_NUM;count++)
-	{
-		if(buttons[count].pin == button )
-		{
-			bool aux =buttons[count].wasPressed;
-			if(aux)
-				buttons[count].wasPressed = false;
-
-			return aux ;
-		}
-	}
-	return false;
-}
-
-bool wasReleased(pin_t button)
-{
-	int count;
-	for(count=0;count<BUTTON_NUM;count++)
-	{
-		if(buttons[count].pin == button )
-		{
-			bool aux =buttons[count].wasReleased;
-			if(aux)
-				buttons[count].wasReleased = false;
-
-			return aux ;
-		}
-	}
-	return false;
-}
-
-bool wasLkp(pin_t button)
-{
-	int count;
-	for(count=0;count<BUTTON_NUM;count++)
-	{
-		if(buttons[count].pin == button )
-		{
-			bool aux =buttons[count].wasLkp;
-			if(aux)
-				buttons[count].wasLkp = false;
-
-			return aux ;
-		}
-	}
-	return false;
-}
-
-bool buttonConfiguration(pin_t button, int type, int time)
-{
-	int count;
-	//I move through the button arrangement and look for the same pin to reconfigure
-	for(count=0;count<BUTTON_NUM;count++)
-	{
-		if(buttons[count].pin == button)
-		{
-			buttons[count].typefunction=type;
-			if(type == LKP)
-				buttons[count].lkpTime = time;
-			else
-				buttons[count].typeTime = time;
-			return true;
-		}
-	}
-	//if the pin was not there use an empty space
-	for(count=0;count<BUTTON_NUM;count++)
-	{
-		if(buttons[count].pin==0)
-		{
-			buttons[count].pin=button;
-			buttons[count].typefunction=type;
-			if(type == LKP)
-				buttons[count].lkpTime = time;
-			else
-				buttons[count].typeTime = time;
-			return true;
-		}
-	}
-	//if there is no empty space for the value
-	return false;
-}
+static Button_t buttons[BUTTON_NUM];
 
 /*******************************************************************************
  *******************************************************************************
@@ -174,4 +56,102 @@ static void systick_callback(void)
 			}
 		}
 	}
+}
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+void buttonsInit(void)
+{
+	//add buttons to .h
+	SysTick_AddCallback(&systick_callback, 50*SYSTICK_ISR_PERIOD_S);
+}
+
+
+bool wasPressed(pin_t button)
+{
+	int count;
+	for(count=0;count<BUTTON_NUM;count++)
+	{
+		if(buttons[count].pin == button )
+		{
+			bool aux =buttons[count].wasPressed;
+			if(aux)
+				buttons[count].wasPressed = false;
+			return aux ;
+		}
+	}
+
+}
+
+bool wasReleased(pin_t button)
+{
+	int count;
+	for(count=0;count<BUTTON_NUM;count++)
+	{
+		if(buttons[count].pin == button )
+		{
+			bool aux =buttons[count].wasReleased;
+			if(aux)
+				buttons[count].wasReleased = false;
+
+			return aux ;
+		}
+	}
+
+}
+
+bool wasLkp(pin_t button)
+{
+	int count;
+	for(count=0;count<BUTTON_NUM;count++)
+	{
+		if(buttons[count].pin == button )
+		{
+			bool aux =buttons[count].wasLkp;
+			if(aux)
+				buttons[count].wasLkp = false;
+
+			return aux ;
+		}
+	}
+}
+
+
+bool buttonConfiguration(pin_t button, int type, int time)
+{
+	int count;
+	//I move through the button arrangement and look for the same pin to reconfigure
+	for(count=0;count<BUTTON_NUM;count++)
+	{
+		if(buttons[count].pin == button)
+		{
+			buttons[count].typefunction=type;
+			if(type == LKP)
+				buttons[count].lkpTime = time;
+			else
+				buttons[count].typeTime = time;
+			return true;
+		}
+	}
+	//if the pin was not there use an empty space
+	for(count=0;count<BUTTON_NUM;count++)
+	{
+		if(buttons[count].pin==0)
+		{
+			buttons[count].pin=button;
+			buttons[count].typefunction=type;
+			if(type == LKP)
+				buttons[count].lkpTime = time;
+			else
+				buttons[count].typeTime = time;
+			return true;
+		}
+	}
+	//if there is no empty space for the value
+	return false;
 }
