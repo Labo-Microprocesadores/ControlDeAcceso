0639e23b1b856bc4b68f255a7a1fb2087b419152
Matias Francois <mfrancois@itba.edu.ar>
2020-10-15 19:28:24 -0300

    more complete usr_state

--- source/FSM/States/usr_state.c
+++ source/FSM/States/usr_state.c
@@ -1,4 +1,4 @@
-/***************************************************************************//**
+/***************************************************************************/ /**
   @file     usr_state.c
   @brief    User state functions.
   @author   Grupo 2 - Lab de Micros
@@ -9,8 +9,39 @@
  ******************************************************************************/
 #include "usr_state.h"
 
-
+/*******************************************************************************
+ * GLOBAL VARIABLES WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+static char *options[OPTIONS_ARRAY_SIZE] = {"OPEN", "CONFIG"};
+static uint8_t currentOptionIndex = 0;
 
 /*******************************************************************************
  * FUNCTIONS WITH GLOBAL SCOPE
  ******************************************************************************/
+
+void nextOption(void)
+{
+    if (currentOptionIndex == OPTIONS_ARRAY_SIZE - 1)
+        currentOptionIndex = 0;
+    else
+        currentOptionIndex++;
+}
+
+void previousOption(void)
+{
+    if (currentOptionIndex == 0)
+        currentOptionIndex = OPTIONS_ARRAY_SIZE - 1;
+    else
+        currentOptionIndex--;
+}
+
+void selectOption(void)
+{
+    switch (currentOptionIndex) //TODO: Add events to the queue.
+    {
+    case 0:
+        break;
+    case 1:
+        break;
+    }
+}
--- source/FSM/States/usr_state.h
+++ source/FSM/States/usr_state.h
@@ -1,14 +1,33 @@
-/***************************************************************************//**
+/***************************************************************************/ /**
   @file     usr_state.h
   @brief    User state header
   @author   Grupo 2 - Lab de Micros
  ******************************************************************************/
 #ifndef USER_STATE_H
 #define USER_STATE_H
-
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+#include <stdint.h>
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+#define OPTIONS_ARRAY_SIZE 2
 /*******************************************************************************
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
+/**
+ * @brief Shows the previous option
+ */
+void nextOption(void);
+/**
+ * @brief Shows the next option.
+ */
+void previousOption(void);
 
+/**
+ * @brief Selects the current option. Adds an event to the event queue of the FSM.
+ */
+void selectOption(void);
 
 #endif /* USER_STATE_H */
