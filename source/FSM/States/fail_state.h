b4ecf90634b96a4a71de203c3821b26f47281382
Matias Francois <mfrancois@itba.edu.ar>
2020-10-15 19:24:06 -0300

    fail state started

---
+++ source/FSM/States/fail_state.h
@@ -0,0 +1,18 @@
+/***************************************************************************//**
+  @file     fail_state.h
+  @brief    fail state header.
+  @author   Grupo 2 - Lab de Micros
+ ******************************************************************************/
+
+#ifndef FAIL_STATE_H_
+#define FAIL_STATE_H_
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
+ ******************************************************************************/
+
+void blockUsr(void);
+void hasAttemptsLeft(void);
+
+#endif /* FAIL_STATE_H_*/
+ 
