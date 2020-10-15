2765b329e1350fdcda6bf0fddf5435b4ecc32e5a
Matias Francois <mfrancois@itba.edu.ar>
2020-10-15 19:25:52 -0300

    main manu updated

--- source/FSM/States/main_menu_state.c
+++ source/FSM/States/main_menu_state.c
@@ -1,4 +1,4 @@
-/***************************************************************************//**
+/***************************************************************************/ /**
   @file     main_menu_state.c
   @brief    Main menu state functions.
   @author   Grupo 2 - Lab de Micros
@@ -9,12 +9,20 @@
  ******************************************************************************/
 #include "main_menu_state.h"
 
-
-
 /*******************************************************************************
  * FUNCTIONS WITH GLOBAL SCOPE
  ******************************************************************************/
 void showWelcomeAnimation(void)
 {
-    //TODO: Show animation indicating "Welcome, pass your id card or click the button to enter it manually" or similar.
+    //TODO: Show animation indicating "Welcome, swipe your id card or click the button to enter it manually" or similar.
+}
+
+void initLogin(void)
+{
+    //TODO: Show animation indicating "Enter ID" or similar, all the neccesary initialization is done by default, switches to id state.
+}
+
+void cardSwipe(void)
+{
+    //TODO: Show animation indicating "YOUR id is 1123123" or similar,recover id from card (no se si esta se hace aca pero bueno), and compare with database. Define wether this event switches to id state or pin state.
 }
--- source/FSM/States/main_menu_state.h
+++ source/FSM/States/main_menu_state.h
@@ -1,4 +1,4 @@
-/***************************************************************************//**
+/***************************************************************************/ /**
   @file     main_menu_state.h
   @brief    Main menu state header
   @author   Grupo 2 - Lab de Micros
@@ -10,7 +10,7 @@
  * FUNCTION PROTOTYPES WITH GLOBAL SCOPE
  ******************************************************************************/
 void showWelcomeAnimation(void);
-
-
+void initLogin(void);
+void cardRead(void);
 
 #endif /* MAIN_MENU_STATE_H */
